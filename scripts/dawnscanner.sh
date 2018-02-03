f377cfe60bfba6b00115799421296b1b93608dcb
Gaurab Bhattacharjee <gaurab@checkr.com>
2018-02-02 16:15:01 -0800

    updated commands for notifying explicitly about vulnerabilities

--- scripts/dawnscanner.sh
+++ scripts/dawnscanner.sh
@@ -3,5 +3,5 @@
 # Script for running Dawnscanner tests
 # Dawnscanner is a security scanner https://github.com/presidentbeef/brakeman.
 
-gem install --no-rdoc --no-ri dawnscanner
-dawn --exit-on-warn .
+# gem install --no-rdoc --no-ri dawnscanner
+dawn --exit-on-warn railsgoat/
